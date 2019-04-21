02e2d301c6539f01495da0c39cbcae05f6856208
Afshin Hoseini <afshin.hoseini@gmail.com>
2019-04-21 23:09:00 +0430

    Task 1 of #1

---
+++ LimberTabbar/LimberTabbar/Tabbar/Item/AHLimberTabbarItem.swift
@@ -0,0 +1,19 @@
+//
+//  AHLimberTabItem.swift
+//  LimberTabbar
+//
+//  Created by Afshin Hoseini on 4/21/19.
+//  Copyright © 2019 Afshin Hoseini. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class AHLimberTabbarItem : UITabBarItem {
+    
+    @IBInspectable
+    var backgroundColor : UIColor?
+    
+    @IBInspectable
+    var iconTintColor : UIColor?
+}
